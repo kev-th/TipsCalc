eca9e0e6151b04b6997bfdee59a2c4ee8a23e295
kev-th <kevinkthaw@gmail.com>
2017-09-21 16:34:10 -0400

    Added SeekBar for Bill Splitting feature

--- app/src/main/java/com/example/android/tipscalc/MainActivity.java
+++ app/src/main/java/com/example/android/tipscalc/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     EditText costBox;
     EditText percentageBox;
     Button button;
+    SeekBar seekBar;
+    TextView seekBarIndicator;
 
 
     @Override
@@ -24,6 +27,31 @@ public class MainActivity extends AppCompatActivity {
         costBox = (EditText) findViewById(R.id.cost);
         percentageBox = (EditText) findViewById(R.id.percentage);
         button = (Button) findViewById(R.id.calculate);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBarIndicator = (TextView) findViewById(R.id.seekBarIndicator);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                progress = i;
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                seekBarIndicator.setText((progress+1) + "/" + (seekBar.getMax()+1));
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                seekBarIndicator.setText((progress+1) + "/" + (seekBar.getMax()+1));
+
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,22 +75,39 @@ public class MainActivity extends AppCompatActivity {
         percentage = Double.parseDouble(percentageBox.getText().toString());
 
 
+
+
         // fields = validate(new EditText[]{costBox,percentageBox});
 
         displayAmount(runCalculation(cost, percentage));
+        if((seekBar.getProgress()+1) > 1)
+        displaySplitAmount(runSplitCalculation(cost,percentage));
+
     }
 
 
     public double runCalculation(double cost, double percentage) {
         return cost * (percentage * 0.01);
 
+    }
+
+    public double runSplitCalculation(double cost, double percentage) {
+        return (runCalculation(cost, percentage)+cost)/(seekBar.getProgress()+1);
 
     }
 
     public void displayAmount(double number) {
         String formattedResult = String.format("%.02f", number);
         TextView resultAmount = (TextView) findViewById(R.id.result);
-        resultAmount.setText("Your amount is: " + formattedResult);
+        resultAmount.setText("Your tip amount is: $" + formattedResult);
+
+    }
+
+    public void displaySplitAmount(double number){
+        String formattedResult = String.format("%.02f", number);
+        TextView resultAmount = (TextView) findViewById(R.id.splitResult);
+        resultAmount.setText("Total amount for each person is: $" + formattedResult);
+
     }
 
     private boolean validate(EditText[] fields) {
